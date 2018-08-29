9bc222ff4984b054f48aef9d92c1a9905abe7f4b
Blake <harrisblake3@gmail.com>
2018-08-29 15:28:32 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,23 +31,46 @@ var checkForMatch = function () {
 }
 };
 
-
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log('User flipped ' + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId].rank);
 
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!')
-	} else {
-		alert('Sorry, try again.')
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a match!')
+		} else {
+			alert('Sorry, try again.')
+		}
 	}
-}
 };
-console.log(flipCard(0));
-console.log(flipCard(2));
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', cards[i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+console.log(createBoard());
+
+
+
+
+
+
+
+
+
+
 
 
