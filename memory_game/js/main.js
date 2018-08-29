c237eec29846bb090370a1dc441ba9dcec797777
Blake <harrisblake3@gmail.com>
2018-08-29 00:57:41 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+ 	},
+ 	{
+ 		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+ 		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+ 		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
 
 var cardsInPlay = [];
 
@@ -10,11 +31,14 @@ var checkForMatch = function () {
 }
 };
 
+
 var flipCard = function (cardId) {
-	console.log('User flipped ' + cards[cardId]);
+	console.log('User flipped ' + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!')
@@ -23,7 +47,7 @@ if (cardsInPlay.length === 2) {
 	}
 }
 };
-
 console.log(flipCard(0));
-console.log(flipCard(1));
+console.log(flipCard(2));
+
 
